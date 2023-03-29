6066b15831f3d98c77bd01b0939d125256ceb500
Arpiii2209 <93611210+Arpiii2209@users.noreply.github.com>
2023-03-29 18:21:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Visualiación de Datos (Universidad Internacional de Valencia)</h1>`;
